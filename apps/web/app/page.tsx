528ea33e41b5f723b8cb36a9758d0e6f1b60eef7
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-07 18:52:07 +0500

    Feature: Make app-sidebar reusable

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,25 @@
 import { SidebarProvider, SidebarTrigger } from "@repo/ui/components/sidebar";
 import AppSidebar from "@repo/ui/components/app-sidebar";
+import { Home as HomeIcon, Plus } from "lucide-react";
+
+const USER_PAGE_LINKS = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "New Sentiment",
+    url: "/new-sentiment",
+    icon: Plus,
+  },
+];
 
 export default function Home() {
   return (
     <main>
       <SidebarProvider>
-        <AppSidebar />
+        <AppSidebar items={USER_PAGE_LINKS} />
         <main>
           <SidebarTrigger />
         </main>
