8e1f98edb7b85010ca9069cd1c2460f1442bfb02
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-07 19:25:22 +0500

    Feature: Create 404 page

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Ghost } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@repo/ui/components/button";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+      <Ghost className="w-20 h-20 text-muted-foreground mb-6" />
+      <h1 className="text-4xl font-bold mb-2">404 - Page not found</h1>
+      <p className="text-muted-foreground mb-6">
+        Sorry, we couldn’t find the page you were looking for.
+      </p>
+      <div className="flex gap-4 flex-wrap justify-center">
+        <Link href="/">
+          <Button>Go back home</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
