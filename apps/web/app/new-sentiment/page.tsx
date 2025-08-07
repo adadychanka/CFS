e5d8806dfcecb83b74e9ab21090580770503832a
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-07 19:26:34 +0500

    Feature: Create sample new sentiment page

---
+++ apps/web/app/new-sentiment/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@repo/ui/components/header";
+
+const Page = () => {
+  return (
+    <>
+      <Header title="New Sentiment" />
+    </>
+  );
+};
+
+export default Page;
