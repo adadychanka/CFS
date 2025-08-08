4d6b6100112e577663e378ea023ef37ce1909a5f
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-08 15:26:52 +0500

    Feature: Create tabs for manual and file upload forms

--- apps/web/app/new-sentiment/page.tsx
+++ apps/web/app/new-sentiment/page.tsx
@@ -6,11 +6,31 @@ export const metadata: Metadata = {
   description:
     "Upload and analyze user reviews from text or CSV files to generate sentiment insights.",
 };
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@repo/ui/components/tabs";
 
 const Page = () => {
   return (
     <>
       <Header title="New Sentiment" />
+      <div className="p-4">
+        <Tabs defaultValue="manual">
+          <TabsList>
+            <TabsTrigger value="manual">Manual entry</TabsTrigger>
+            <TabsTrigger value="file">File upload</TabsTrigger>
+          </TabsList>
+          <TabsContent value="manual">
+            <h2>Manul form will here</h2>
+          </TabsContent>
+          <TabsContent value="file">
+            <h2>File upload will here</h2>
+          </TabsContent>
+        </Tabs>
+      </div>
     </>
   );
 };
