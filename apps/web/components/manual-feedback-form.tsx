81932fffbea62790c9f1843a3949f0fa32c222f0
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-08 18:10:08 +0500

    Feature: Create manual feedback form with textarea

---
+++ apps/web/components/manual-feedback-form.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@repo/ui/components/form";
+import { Textarea } from "@repo/ui/components/textarea";
+import { useForm } from "react-hook-form";
+
+const FormSchema = z.object({
+  feedbacks: z.string().min(30, {
+    message: "Bio must be at least 30 characters.",
+  }),
+});
+
+const ManualFeedbackForm = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  return (
+    <Form {...form}>
+      <form className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="feedbacks"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Feedbacks</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Paste your feedbacks here. Each line considered as separate item."
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default ManualFeedbackForm;
