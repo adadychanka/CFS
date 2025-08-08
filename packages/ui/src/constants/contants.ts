61fa534c0364364df01d3077afa8f3dffe9b0c92
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-08 11:03:19 +0500

    Feature: Render different sidebars based on user role

--- packages/ui/src/constants/contants.ts
+++ packages/ui/src/constants/contants.ts
@@ -1,4 +1,6 @@
-import { Home as HomeIcon, Plus } from "lucide-react";
+import { Home as HomeIcon, Plus, Settings, TriangleAlert } from "lucide-react";
+
+export const USER_ROLE = "admin";
 
 export const USER_PAGE_LINKS = [
   {
@@ -10,5 +12,23 @@ export const USER_PAGE_LINKS = [
     title: "New Sentiment",
     url: "/new-sentiment",
     icon: Plus,
+  },
+];
+
+export const ADMIN_PAGE_LINKS = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Alerts",
+    url: "/alerts",
+    icon: TriangleAlert,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
   },
 ];
