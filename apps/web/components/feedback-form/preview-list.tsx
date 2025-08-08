66c903a3a844f8f336b47ca92fef487895f6b8e3
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-08 20:28:05 +0500

    Feature: Show correct items length

--- apps/web/components/feedback-form/preview-list.tsx
+++ apps/web/components/feedback-form/preview-list.tsx
@@ -1,9 +1,12 @@
 import PreviewListTable from "@/components/feedback-form/preview-list-table";
+import { TEST_PREVIEW_FEEDBACKS } from "@repo/ui/constants/contants";
 
 const PreviewList = () => {
   return (
     <div>
-      <p className="pb-2 font-medium">18 items to preview</p>
+      <p className="pb-2 font-medium">
+        {TEST_PREVIEW_FEEDBACKS.length} items to preview
+      </p>
       <PreviewListTable />
     </div>
   );
