ac1240d86c3826155990994c713e7bb8906f79ca
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-08 20:26:26 +0500

    Style: Customize preview table

--- apps/web/components/feedback-form/preview-list.tsx
+++ apps/web/components/feedback-form/preview-list.tsx
@@ -3,7 +3,7 @@ import PreviewListTable from "@/components/feedback-form/preview-list-table";
 const PreviewList = () => {
   return (
     <div>
-      <p>18 items to preview</p>
+      <p className="pb-2 font-medium">18 items to preview</p>
       <PreviewListTable />
     </div>
   );
