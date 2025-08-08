eff1d4a061afd5a20816c857c6aeccc9ee807ce7
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-08 15:26:52 +0500

    Feature: Create tabs for manual and file upload forms

---
+++ packages/ui/src/constants/contants.ts
@@ -0,0 +1,34 @@
+import { Home as HomeIcon, Plus, Settings, TriangleAlert } from "lucide-react";
+
+export const USER_ROLE: "user" | "admin" = "user";
+
+export const USER_PAGE_LINKS = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "New Sentiment",
+    url: "/new-sentiment",
+    icon: Plus,
+  },
+];
+
+export const ADMIN_PAGE_LINKS = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Alerts",
+    url: "/alerts",
+    icon: TriangleAlert,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
