aa8433d2c1e92435eb1bf8f7e07c3a6d9784ae19
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-08 11:07:49 +0500

    Feature: Create admin alerts and settings page

---
+++ apps/web/app/alerts/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@repo/ui/components/header";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <Header title="Alerts" />
+    </>
+  );
+};
+
+export default Page;
