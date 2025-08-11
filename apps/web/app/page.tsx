4ee20a949a92ab72c2aff7ce6a4ab4d7e78c2595
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-11 16:08:58 +0500

    Feature: Add ui sections for charts and table

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,16 @@
 import Header from "@repo/ui/components/header";
+import SentimentsTable from "@/components/user-sentiments/SentimentsTable";
 
 export default function Home() {
   return (
     <>
       <Header title="Dashboard" />
+      <div className="w-full max-w-[1280px] mx-auto p-4">
+        <section className="w-full h-64 mb-8 flex items-center justify-center bg-neutral-100 rounded-md">
+          Charts :)
+        </section>
+        <SentimentsTable />
+      </div>
     </>
   );
 }
