acf3f2bb3f5a87df7275d82d6283a96a6b036c5a
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-11 16:08:58 +0500

    Feature: Add ui sections for charts and table

---
+++ apps/web/components/user-sentiments/SentimentsTable.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SentimentsTable = () => {
+  return (
+    <section className="w-full h-64 flex items-center justify-center bg-neutral-100 rounded-md">
+      Tables :0
+    </section>
+  );
+};
+
+export default SentimentsTable;
