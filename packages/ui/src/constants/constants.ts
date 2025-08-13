48744e17c923ed59fec3f623e0e609bc1d1f4680
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-13 16:59:37 +0500

    Fix: Close nav on mobile

--- packages/ui/src/constants/constants.ts
+++ packages/ui/src/constants/constants.ts
@@ -1,17 +1,15 @@
-import { Home as HomeIcon, Plus, Settings, TriangleAlert } from "lucide-react";
-
 export const USER_ROLE: "user" | "admin" = "user";
 
 export const USER_PAGE_LINKS = [
   {
     title: "Dashboard",
     url: "/",
-    icon: HomeIcon,
+    // icon: HomeIcon,
   },
   {
     title: "New Sentiment",
     url: "/new-sentiment",
-    icon: Plus,
+    // icon: Plus,
   },
 ];
 
@@ -19,17 +17,17 @@ export const ADMIN_PAGE_LINKS = [
   {
     title: "Dashboard",
     url: "/",
-    icon: HomeIcon,
+    // icon: HomeIcon,
   },
   {
     title: "Alerts",
     url: "/alerts",
-    icon: TriangleAlert,
+    // icon: TriangleAlert,
   },
   {
     title: "Settings",
     url: "/settings",
-    icon: Settings,
+    // icon: Settings,
   },
 ];
 
