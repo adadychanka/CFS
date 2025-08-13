c7b5f603b536c32f4c9f7162bebe242abf15f211
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-14 00:16:24 +0500

    Feature: Add simple pagination with custom components

--- apps/web/components/user-feedback/feedback-table.tsx
+++ apps/web/components/user-feedback/feedback-table.tsx
@@ -17,10 +17,25 @@ import FeedbackBadge from "@/components/user-feedback/feedback-badge";
 import { formatDateByYearMonthDays } from "@/utils/dateUtils";
 import SkeletonFeedbackItem from "@/components/user-feedback/skeleton-feedback-item";
 import NoFeedbackMessage from "@/components/user-feedback/no-feedback-message";
+import { useSearchParams } from "next/navigation";
+import { PaginationLink } from "@repo/ui/components/pagination";
+import Link from "next/link";
+import {
+  PaginationNextWithLink,
+  PaginationPreviousWithLink,
+} from "@/components/user-feedback/paginationControls";
 
 const FeedbackTable = () => {
-  // TODO: connect to API in the future
   const [isLoading, setIsLoading] = useState(true);
+  const searchParams = useSearchParams();
+  const totalPages = 10;
+
+  const currentPage = Number(searchParams.get("page")) || 1;
+  const getPageLink = (page: number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", String(page));
+    return `?${params.toString()}`;
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -29,46 +44,70 @@ const FeedbackTable = () => {
   }, []);
 
   return (
-    <div className="overflow-x-auto rounded-md border max-h-[824px]">
-      <Table className="min-w-[600px]">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[300px]">Summary</TableHead>
-            <TableHead className="w-[100px] text-center">Sentiment</TableHead>
-            <TableHead className="w-[100px] text-center">Confidence</TableHead>
-            <TableHead className="min-w-[200px]">Content</TableHead>
-            <TableHead className="w-[120px]">Created At</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {isLoading ? (
-            [...Array(FEEDBACK_PAGE_LIMIT)].map((_, i) => (
-              <SkeletonFeedbackItem key={i} />
-            ))
-          ) : FAKE_PROCESSED_FEEDBACK.length === 0 ? (
-            <NoFeedbackMessage />
-          ) : (
-            FAKE_PROCESSED_FEEDBACK.slice(0, FEEDBACK_PAGE_LIMIT).map((fd) => (
-              <TableRow key={fd.id} className="odd:bg-muted/50">
-                <TableCell>{fd.summary}</TableCell>
-                <TableCell className="text-center">
-                  <FeedbackBadge sentiment={fd.sentiment} />
-                </TableCell>
-                <TableCell className="text-center">{fd.confidence}%</TableCell>
-                <TableCell
-                  className="max-w-[300px] truncate"
-                  title={fd.content}
-                >
-                  {fd.content}
-                </TableCell>
-                <TableCell>
-                  {formatDateByYearMonthDays(fd.created_at)}
-                </TableCell>
-              </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
+    <div className="flex flex-col gap-8">
+      {totalPages > 1 && (
+        <div className="mt-4 flex justify-center gap-2">
+          <PaginationPreviousWithLink
+            asChild
+            href={getPageLink(currentPage - 1)}
+          />
+
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((num) => (
+            <PaginationLink key={num} isActive={currentPage === num} asChild>
+              <Link href={getPageLink(num)}>{num}</Link>
+            </PaginationLink>
+          ))}
+
+          <PaginationNextWithLink href={getPageLink(currentPage + 1)} />
+        </div>
+      )}
+      <div className="overflow-x-auto rounded-md border max-h-[824px]">
+        <Table className="min-w-[600px]">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[300px]">Summary</TableHead>
+              <TableHead className="w-[100px] text-center">Sentiment</TableHead>
+              <TableHead className="w-[100px] text-center">
+                Confidence
+              </TableHead>
+              <TableHead className="min-w-[200px]">Content</TableHead>
+              <TableHead className="w-[120px]">Created At</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {isLoading ? (
+              [...Array(FEEDBACK_PAGE_LIMIT)].map((_, i) => (
+                <SkeletonFeedbackItem key={i} />
+              ))
+            ) : FAKE_PROCESSED_FEEDBACK.length === 0 ? (
+              <NoFeedbackMessage />
+            ) : (
+              FAKE_PROCESSED_FEEDBACK.slice(0, FEEDBACK_PAGE_LIMIT).map(
+                (fd) => (
+                  <TableRow key={fd.id} className="odd:bg-muted/50">
+                    <TableCell>{fd.summary}</TableCell>
+                    <TableCell className="text-center">
+                      <FeedbackBadge sentiment={fd.sentiment} />
+                    </TableCell>
+                    <TableCell className="text-center">
+                      {fd.confidence}%
+                    </TableCell>
+                    <TableCell
+                      className="max-w-[300px] truncate"
+                      title={fd.content}
+                    >
+                      {fd.content}
+                    </TableCell>
+                    <TableCell>
+                      {formatDateByYearMonthDays(fd.created_at)}
+                    </TableCell>
+                  </TableRow>
+                ),
+              )
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
