35950da4de9b7b1a6c6f8ebb89cb3d0a2a662a21
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-18 23:36:59 +0500

    Style: Change charts responsiveness

--- apps/web/components/e-charts/new-chart-wrapper.tsx
+++ apps/web/components/e-charts/new-chart-wrapper.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react";
-import { cn } from "@repo/ui/lib/utils";
+import {ReactNode} from "react";
+import {cn} from "@repo/ui/lib/utils";
 
 const NewChartWrapper = ({
   className,
@@ -11,7 +11,7 @@ const NewChartWrapper = ({
   return (
     <div
       className={cn(
-        "w-full aspect-[4/3] border-1 rounded-md p-2 @lg:aspect-video @2xl:aspect-[4/3] @5xl:col-span-1 @5xl:aspect-[4/3]",
+        "w-full border-1 rounded-md p-2  aspect-square @xs:aspect-[4/3] @lg:aspect-video @2xl:aspect-[4/3] @4xl:aspect-video @5xl:col-span-1 @5xl:aspect-[4/3]",
         className,
       )}
     >
