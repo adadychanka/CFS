b5b0e75ef0f913aff64994c63ef896911fc430f9
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-18 15:59:04 +0500

    Feature: Create admin with metadata

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,38 @@
+import { type Metadata } from "next";
+import { METADATA } from "@/constants/metadata";
+import Header from "@repo/ui/components/header";
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+  description:
+    "Monitor API usage, track errors, and analyze daily uploads with interactive charts. Manage users with suspension and access control tools.",
+  keywords: [
+    "admin dashboard",
+    "API usage",
+    "error monitoring",
+    "uploads tracking",
+    "user management",
+    "suspend users",
+    "limit users",
+    "admin tools",
+    "analytics dashboard",
+  ],
+  openGraph: {
+    title: "Admin Dashboard",
+    description:
+      "Powerful admin dashboard to monitor API usage, errors, and uploads with daily charts, plus manage users through suspension and access controls.",
+    url: METADATA.URL,
+    siteName: METADATA.siteName,
+    type: "website",
+  },
+};
+
+const Page = () => {
+  return (
+    <div>
+      <Header title="Admin Dashboard" />
+    </div>
+  );
+};
+
+export default Page;
