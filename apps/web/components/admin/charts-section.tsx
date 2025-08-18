f71889b46fd923f1ab30c3fb04e5ce82dac16213
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-18 23:39:08 +0500

    Style: Change charts responsiveness

--- apps/web/components/admin/charts-section.tsx
+++ apps/web/components/admin/charts-section.tsx
@@ -7,12 +7,9 @@ import AnalyzedSentiments from "@/components/e-charts/analyzed-sentiments";
 const ChartsSection = () => {
   return (
     <div className="p-4 grid grid-cols-1 @2xl:grid-cols-2 @5xl:grid-cols-3 gap-4">
-
-
       <ApiUsageChart />
       <AnalyzedSentiments />
       <ErrorsChart />
-
     </div>
   );
 };
