9e6acdd5ebcdb0b0885ee5d44bbce8d7e2a573d5
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-20 22:37:01 +0500

    Feature: Refactor code

---
+++ apps/web/lib/errors.ts
@@ -0,0 +1,11 @@
+export class FetchError extends Error {
+  status: number;
+  info?: unknown;
+
+  constructor(message: string, status: number, info?: unknown) {
+    super(message);
+    this.name = "FetchError";
+    this.status = status;
+    this.info = info;
+  }
+}
