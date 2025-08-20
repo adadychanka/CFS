d4a6b8a2d15ceb098cb5a70c099ffc02e30f77e1
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-20 15:57:45 +0500

    Feature: Change schema and types

--- apps/web/app/api/feedback/route.ts
+++ apps/web/app/api/feedback/route.ts
@@ -1,15 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-
-// type PaginationMeta = {
-//   totalItems: number;
-//   totalPages: number;
-//   currentPage: number;
-// };
+import { SentimentAnalysisResult } from "@/types/sentiment-analysis-result";
 
 export async function GET(req: NextRequest) {
   try {
-    // TODO: Connect to real API ;(
-    // TODO: connect pagination limits to the API meta
+    // TODO:
+    //  Connect to real API ;(
+    //  connect pagination limits to the API meta
+    //  401 is not caught
+    //  test API has no pagination
+
     const page = parseInt(req.nextUrl.searchParams.get("page") || "1", 10);
     const limit = parseInt(req.nextUrl.searchParams.get("limit") || "20", 10);
 
@@ -19,17 +18,17 @@ export async function GET(req: NextRequest) {
       throw new Error("Random simulated error");
     }
 
-    // TODO: 401 is not caught
     const res = await fetch(
-      `https://dummyjson.com/products?limit=${limit}&skip=${skip}`,
+      `https://dummyjson.com/c/c822-5738-40dc-95b7?limit=${limit}&skip=${skip}`,
     );
     if (!res.ok) throw new Error(`Failed to fetch products: ${res.status}`);
 
-    const feedback = await res.json();
+    const feedback: SentimentAnalysisResult[] = await res.json();
 
     return NextResponse.json({ success: true, feedback });
   } catch (e: unknown) {
-    // TODO: only 500 caught,
+    // TODO:
+    //  only 500 caught,
     const message = e instanceof Error ? e.message : "Internal Server Error";
 
     return NextResponse.json({ message }, { status: 500 });
--- apps/web/types/sentiment-analysis-result.ts
+++ apps/web/types/sentiment-analysis-result.ts
@@ -1,12 +1,16 @@
 export type Sentiment = "positive" | "neutral" | "negative" | "unknown";
 
 export type SentimentAnalysisResult = {
-  id: string;
   content: string;
-  sentiment: Sentiment;
+  sentiment: "positive" | "neutral" | "negative" | "unknown";
   confidence: number;
   summary: string;
-  created_at: string;
+  userId: string;
+  fileId: string | null;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
 };
 
 export type SentimentAnalysisGroup = {
