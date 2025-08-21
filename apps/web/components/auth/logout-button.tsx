2ae5074e2f4e708c30e1ce9b66d4cfb201f64957
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-21 11:56:35 +0500

    Feature: Connect logout to client auth

--- apps/web/components/auth/logout-button.tsx
+++ apps/web/components/auth/logout-button.tsx
@@ -2,29 +2,11 @@
 
 import { LogOut } from "lucide-react";
 import { SidebarMenuButton } from "@repo/ui/components/sidebar";
-import { useRouter } from "next/navigation";
-import { AUTH_MESSAGES } from "@/constants/response-messages";
+import { signOut } from "next-auth/react";
 
 const LogoutButton = () => {
-  const router = useRouter();
-
-  // Logout user, even works without the token
   const handleLogout = async () => {
-    try {
-      const token = localStorage.getItem("token");
-
-      if (token) {
-        await fetch("/api/auth/logout", {
-          method: "POST",
-          headers: { Authorization: `Bearer ${token}` },
-        });
-      }
-    } catch (e) {
-      console.error(AUTH_MESSAGES.LOGOUT.ERROR, e);
-    } finally {
-      localStorage.removeItem("token");
-      router.replace("/log-in");
-    }
+    await signOut({ redirectTo: "/log-in" });
   };
 
   return (
