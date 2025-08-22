82ee73003a32d9d5a93916360815576253dbfbf8
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-22 15:22:54 +0500

    Feature: Save data in local storage

--- apps/web/components/feedback-form/manual-feedback-tab.tsx
+++ apps/web/components/feedback-form/manual-feedback-tab.tsx
@@ -2,9 +2,10 @@
 
 import ManualFeedbackForm from "@/components/feedback-form/manual-feedback-form";
 import PreviewListSection from "@/components/feedback-form/preview-list/preview-list-section";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import ManualFeedbackSubmitButton from "@/components/feedback-form/manual-feedback-submit-button";
 import { FEEDBACK_MAX_ITEMS } from "@/constants/constants";
+import { useSessionStorage } from "@/hooks/useSessionStorage";
 
 export type PreviewFeedback = {
   id: string;
@@ -12,15 +13,24 @@ export type PreviewFeedback = {
 };
 
 const ManualFeedbackTab = () => {
-  const [feedback, setFeedback] = useState<PreviewFeedback[]>([]);
+  const [feedback, setFeedback] = useSessionStorage<PreviewFeedback[]>(
+    "preview-feedback",
+    [],
+  );
 
-  const handleAddFeedback = useCallback((feedback: PreviewFeedback[]) => {
-    setFeedback((prev) => [...prev, ...feedback]);
-  }, []);
+  const handleAddFeedback = useCallback(
+    (feedback: PreviewFeedback[]) => {
+      setFeedback((prev) => [...prev, ...feedback]);
+    },
+    [setFeedback],
+  );
 
-  const handleRemoveFeedback = useCallback((id: string) => {
-    setFeedback((prev) => prev.filter((e) => e.id !== id));
-  }, []);
+  const handleRemoveFeedback = useCallback(
+    (id: string) => {
+      setFeedback((prev) => prev.filter((e) => e.id !== id));
+    },
+    [setFeedback],
+  );
 
   const handleClearFeedback = useCallback(() => {
     if (feedback.length > FEEDBACK_MAX_ITEMS) {
@@ -28,7 +38,7 @@ const ManualFeedbackTab = () => {
     } else {
       setFeedback([]);
     }
-  }, [feedback.length]);
+  }, [feedback.length, setFeedback]);
 
   return (
     <div>
