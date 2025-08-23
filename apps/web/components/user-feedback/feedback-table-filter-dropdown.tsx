30af2b1fcfacfaec24f7b37eb46095a959f10b3c
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-23 16:20:30 +0500

    Feature: Save selected filter

--- apps/web/components/user-feedback/feedback-table-filter-dropdown.tsx
+++ apps/web/components/user-feedback/feedback-table-filter-dropdown.tsx
@@ -2,19 +2,39 @@
 
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "@repo/ui/components/dropdown-menu";
 import { Button } from "@repo/ui/components/button";
-import { useState } from "react";
+import { useEffect } from "react";
 import { Filter } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { FEEDBACK_FILTERS } from "@/constants/constants";
 
 export function FeedbackTableFilterDropdown() {
-  const [showStatusBar, setShowStatusBar] = useState(true);
-  const [showActivityBar, setShowActivityBar] = useState(false);
-  const [showPanel, setShowPanel] = useState(false);
-  const [filterNegative, setFilterNegative] = useState(false);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const filterOnURLQuery = searchParams.get("sentiment") || "no-filter";
+  const selectedFilter = FEEDBACK_FILTERS.includes(filterOnURLQuery)
+    ? filterOnURLQuery
+    : "no-filter";
+
+  useEffect(() => {
+    if (!FEEDBACK_FILTERS.includes(filterOnURLQuery)) {
+      const params = new URLSearchParams(searchParams);
+      params.set("sentiment", "no-filter");
+      router.replace(`?${params.toString()}`);
+    }
+  }, [filterOnURLQuery, searchParams, router]);
+
+  const handleChange = (newValue: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("sentiment", newValue);
+    router.push(`?${params.toString()}`);
+  };
 
   return (
     <DropdownMenu>
@@ -25,31 +45,66 @@ export function FeedbackTableFilterDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuCheckboxItem
-          checked={showStatusBar}
-          onCheckedChange={setShowStatusBar}
+        <DropdownMenuRadioGroup
+          value={selectedFilter}
+          onValueChange={handleChange}
         >
-          Positive
-        </DropdownMenuCheckboxItem>
-        <DropdownMenuCheckboxItem
-          checked={showActivityBar}
-          onCheckedChange={setShowActivityBar}
-        >
-          Neutral
-        </DropdownMenuCheckboxItem>
-        <DropdownMenuCheckboxItem
-          checked={showPanel}
-          onCheckedChange={setShowPanel}
-        >
-          Negative
-        </DropdownMenuCheckboxItem>
-        <DropdownMenuCheckboxItem
-          checked={filterNegative}
-          onCheckedChange={setFilterNegative}
-        >
-          Unknow
-        </DropdownMenuCheckboxItem>
+          <DropdownMenuRadioItem value="no-filter">
+            no filter
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="positive">
+            positive
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="neutral">neutral</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="negative">
+            negative
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="unknown">unknown</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
+
+  // NOTE: In case of multi filters are allowed
+  // const [showStatusBar, setShowStatusBar] = useState(true);
+  // const [showActivityBar, setShowActivityBar] = useState(false);
+  // const [showPanel, setShowPanel] = useState(false);
+  // const [filterNegative, setFilterNegative] = useState(false);
+  //
+  // return (
+  //   <DropdownMenu>
+  //     <DropdownMenuTrigger asChild>
+  //       <Button variant="ghost" size="sm">
+  //         <Filter />
+  //         Sentiment
+  //       </Button>
+  //     </DropdownMenuTrigger>
+  //     <DropdownMenuContent>
+  //       <DropdownMenuCheckboxItem
+  //         checked={showStatusBar}
+  //         onCheckedChange={setShowStatusBar}
+  //       >
+  //         Positive
+  //       </DropdownMenuCheckboxItem>
+  //       <DropdownMenuCheckboxItem
+  //         checked={showActivityBar}
+  //         onCheckedChange={setShowActivityBar}
+  //       >
+  //         Neutral
+  //       </DropdownMenuCheckboxItem>
+  //       <DropdownMenuCheckboxItem
+  //         checked={showPanel}
+  //         onCheckedChange={setShowPanel}
+  //       >
+  //         Negative
+  //       </DropdownMenuCheckboxItem>
+  //       <DropdownMenuCheckboxItem
+  //         checked={filterNegative}
+  //         onCheckedChange={setFilterNegative}
+  //       >
+  //         Unknow
+  //       </DropdownMenuCheckboxItem>
+  //     </DropdownMenuContent>
+  //   </DropdownMenu>
+  // );
 }
