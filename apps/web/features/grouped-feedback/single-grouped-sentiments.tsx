d881cd43a591e3a72d682e518e28af6a2494f03a
Rakhmiddin Asiydinov <rahmiddinasiydinov@gmail.com>
2025-08-25 18:25:46 +0500

    CFS-83: Normalize Using Dynamic Table

--- apps/web/features/grouped-feedback/single-grouped-sentiments.tsx
+++ apps/web/features/grouped-feedback/single-grouped-sentiments.tsx
@@ -1,33 +1,47 @@
 import DynamicFeedbackTable from "@/components/user-feedback/dynamic-feedback-table";
+import useGroupedSentimentTable from "@/hooks/useGroupedSentimentTable";
 import type { GroupedFeedbackDataItems } from "@/types/grouped-feedback";
+import { capitalizeSentence } from "@/utils/capitilize";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@repo/ui/components/accordion";
 import Link from "next/link";
 import React from "react";
 
 type Props = {
   data: GroupedFeedbackDataItems[];
   summary: string;
+  isLoading: boolean;
 };
 
 const FEEDBACK_LIMIT = 10;
 
-function SingleGroupedSentiments({ data, summary }: Props) {
+function SingleGroupedSentiments({ data, summary, isLoading }: Props) {
+  const { tableHeads, tableRows } = useGroupedSentimentTable({ data });
+
   return (
-    <>
-      <DynamicFeedbackTable
-        isLoading={false}
-        feedbackList={data}
-        feedbackLimit={FEEDBACK_LIMIT}
-        isGrouped={true}
-      />
-      {data.length >= FEEDBACK_LIMIT && (
-        <Link
-          href={`/result/${summary}`}
-          className="px-5 py-2 inline-block my-2 rounded-md bg-primary text-primary-foreground shadow-xs hover:bg-primary/90"
-        >
-          View all results
-        </Link>
-      )}
-    </>
+    <AccordionItem key={summary} value={summary}>
+      <AccordionTrigger>{capitalizeSentence(summary)}</AccordionTrigger>
+      <AccordionContent>
+        <DynamicFeedbackTable
+          isLoading={isLoading}
+          feedbackList={data}
+          feedbackLimit={FEEDBACK_LIMIT}
+          tableHeads={tableHeads}
+          tableRows={tableRows}
+        />
+        {data.length >= FEEDBACK_LIMIT && (
+          <Link
+            href={`/result/${summary}`}
+            className="px-5 py-2 inline-block my-2 rounded-md bg-primary text-primary-foreground shadow-xs hover:bg-primary/90"
+          >
+            View all results
+          </Link>
+        )}
+      </AccordionContent>
+    </AccordionItem>
   );
 }
 
