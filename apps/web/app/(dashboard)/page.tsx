10b372e78f921f506dcbe3df25f731f61d0ec03c
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-25 12:39:03 +0500

    Feature: Move the sample mode state to the page

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,29 +1,44 @@
+"use client";
+
 import Header from "@repo/ui/components/header";
 import SwitchTableCollapsedTabs from "@/components/user-feedback/switch-table-collapsed-tabs";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Button } from "@repo/ui/components/button";
-import { SwatchBook } from "lucide-react";
+import { FlaskConical, LogOut } from "lucide-react";
 import DashboardChartWrapper from "@/features/dashboard-chart/dashboard-chart-wrapper";
 
+// TODO: We can create separate component
 export default function Home() {
+  const [isSampleMode, setIsSampleMode] = useState(false);
+
   return (
     <>
       <Header title="Dashboard" />
       <div className="w-full max-w-[1280px] mx-auto p-4">
+        {/*TODO: this can go inside the header*/}
         <div className="pb-8 flex justify-end">
           <Button
             size="sm"
             variant="outline"
             aria-label="Toggle sample mode for user dashboard"
+            onClick={() => setIsSampleMode((prev) => !prev)}
           >
-            <SwatchBook /> Sample mode
+            {isSampleMode ? (
+              <>
+                <FlaskConical /> Enter sample mode
+              </>
+            ) : (
+              <>
+                <LogOut /> Exit sample mode
+              </>
+            )}
           </Button>
         </div>
 
         {/* TODO: need custom loading in the future*/}
         <Suspense fallback={<p>Loading...</p>}>
           <DashboardChartWrapper />
-          <SwitchTableCollapsedTabs />
+          <SwitchTableCollapsedTabs isSampleMode={isSampleMode} />
         </Suspense>
       </div>
     </>
