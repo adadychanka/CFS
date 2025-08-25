d325a79a88d313296f9255718e2e28c5702997ac
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-25 12:07:17 +0500

    Feature: Create sample sample mode

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@repo/ui/components/header";
 import SwitchTableCollapsedTabs from "@/components/user-feedback/switch-table-collapsed-tabs";
 import { Suspense } from "react";
+import { Button } from "@repo/ui/components/button";
+import { SwatchBook } from "lucide-react";
 import DashboardChartWrapper from "@/features/dashboard-chart/dashboard-chart-wrapper";
 import FeedbackPanel from "@/features/feedback-panel/feedback-panel";
 
@@ -9,6 +11,16 @@ export default function Home() {
     <>
       <Header title="Dashboard" />
       <div className="w-full max-w-[1280px] mx-auto p-4">
+        <div className="pb-8 flex justify-end">
+          <Button
+            size="sm"
+            variant="outline"
+            aria-label="Toggle sample mode for user dashboard"
+          >
+            <SwatchBook /> Sample mode
+          </Button>
+        </div>
+
         {/* TODO: need custom loading in the future*/}
         <Suspense fallback={<p>Loading...</p>}>
           <DashboardChartWrapper />
