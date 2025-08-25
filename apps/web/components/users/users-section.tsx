81760bef3e55fdfbb811de2015a455e674c4e17b
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-25 21:00:31 +0500

    Feature: Add pagination to users

--- apps/web/components/users/users-section.tsx
+++ apps/web/components/users/users-section.tsx
@@ -2,6 +2,8 @@ import UsersList from "@/components/users/users-list";
 import { Input } from "@repo/ui/components/input";
 import { Button } from "@repo/ui/components/button";
 import { Search } from "lucide-react";
+import FeedbackTablePagination from "@/components/user-feedback/feedback-table-pagination";
+import { Suspense } from "react";
 
 const UsersSection = () => {
   return (
@@ -13,6 +15,11 @@ const UsersSection = () => {
         </Button>
       </div>
       <UsersList />
+
+      {/* TODO: Need custom loader it seems */}
+      <Suspense fallback={"Loading..."}>
+        <FeedbackTablePagination limit={20} />
+      </Suspense>
     </div>
   );
 };
