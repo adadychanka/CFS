85dd3ee7a67ae83d219df6ce799024e40d9284b0
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-25 19:30:00 +0500

    Feature: Cleanup URL and link on error

--- apps/web/utils/browser-file-downloader.ts
+++ apps/web/utils/browser-file-downloader.ts
@@ -6,17 +6,17 @@
  */
 export const browserFileDownloader = (blob: Blob, filename: string) => {
   const fileURL = URL.createObjectURL(blob);
+  const link = document.createElement("a");
 
   try {
-    const link = document.createElement("a");
     link.href = fileURL;
     link.download = filename;
     document.body.appendChild(link);
     link.click();
-    link.remove();
   } catch {
     console.error("Failed to download file.");
   } finally {
+    link.remove();
     URL.revokeObjectURL(fileURL);
   }
 };
