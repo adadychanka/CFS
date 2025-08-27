e5a803dc52fb7cefc58895452ec34aa249b8c969
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-27 16:16:44 +0500

    Feature: Connect list to admin, show roles, protect admin

--- apps/web/features/users-management/users-section.tsx
+++ apps/web/features/users-management/users-section.tsx
@@ -1,11 +1,45 @@
+"use client";
+
 import UsersList from "@/features/users-management/users-list";
 import { Input } from "@repo/ui/components/input";
 import { Button } from "@repo/ui/components/button";
 import { Search } from "lucide-react";
 import PaginationSection from "@/components/user-feedback/pagination-section";
 import { Suspense } from "react";
+import { FetchError } from "@/lib/errors";
+import useSWR from "swr";
+import type { GetUsersResponse } from "@/types/http";
+import { FEEDBACK_PAGE_LIMIT } from "@/constants/constants";
+import { clientAuthGuard } from "@/utils/client-auth-guard";
+
+export const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new FetchError(
+      data.message || "Something went wrong",
+      res.status,
+      data,
+    );
+  }
+
+  return data;
+};
 
 const UsersSection = () => {
+  const { data, error, isLoading, mutate } = useSWR<GetUsersResponse>(
+    `/api/users?page=${1}&limit=${FEEDBACK_PAGE_LIMIT}`,
+    fetcher,
+    {
+      keepPreviousData: true,
+    },
+  );
+
+  if (error instanceof FetchError) clientAuthGuard(error.status);
+
+  console.log(data && data);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center gap-2">
@@ -15,7 +49,11 @@ const UsersSection = () => {
         </Button>
       </div>
 
-      <UsersList />
+      <UsersList
+        usersList={data?.users || []}
+        isLoading={isLoading}
+        error={error}
+      />
 
       {/* TODO: Need custom loader it seems */}
       <Suspense fallback={"Loading..."}>
