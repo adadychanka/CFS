2981e905af2449bc3e0af9b94003f505aafe5982
Rakhmiddin Asiydinov <rahmiddinasiydinov@gmail.com>
2025-08-27 17:04:32 +0500

    CFS-119: Split User and admin pages

---
+++ apps/web/components/admin/admin-layout-wrapper.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/auth/auth";
+import { redirect } from "next/navigation";
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+async function AdminLayoutWrapper({ children }: Props) {
+  const session = await auth();
+  if (session?.user.role !== "ADMIN") {
+    redirect("/admin/log-in");
+  }
+
+  return <>{children}</>;
+}
+
+export default AdminLayoutWrapper;
