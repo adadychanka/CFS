74443be030a6c972d8169f578b7de2792f421391
Rakhmiddin Asiydinov <rahmiddinasiydinov@gmail.com>
2025-08-28 16:30:10 +0500

    CFS-117: Apply Suggested changes

--- apps/web/app/(admin)/admin/log-in/page.tsx
+++ apps/web/app/(admin)/admin/log-in/page.tsx
@@ -2,15 +2,11 @@ import React from "react";
 
 import { AuthCard } from "@/components/auth/auth-card";
 import AuthCardWrapper from "@/components/auth/auth-card-wrapper";
-import { auth } from "@/auth/auth";
 
 async function Page() {
-  const session = await auth();
-  const isAdmin = session?.user.role === "ADMIN";
-
   return (
     <AuthCardWrapper>
-      <AuthCard variant="admin-sign-in" isAdmin={isAdmin} />
+      <AuthCard variant="admin-sign-in" />
     </AuthCardWrapper>
   );
 }
--- apps/web/components/auth/auth-card.tsx
+++ apps/web/components/auth/auth-card.tsx
@@ -13,12 +13,10 @@ import { getCardContent, type AuthCardVariant } from "@/utils/get-card-content";
 
 type Props = {
   variant: AuthCardVariant;
-  isAdmin?: boolean;
 };
 
-function AuthCard({ variant, isAdmin = false }: Props) {
+function AuthCard({ variant }: Props) {
   const { link, linkText, title } = getCardContent(variant);
-  const isLinkVisible = isAdmin || link !== "/admin/sign-up";
 
   return (
     <Card className="w-full max-w-md shadow-md">
@@ -29,14 +27,12 @@ function AuthCard({ variant, isAdmin = false }: Props) {
         <AuthForm variant={variant} />
       </CardContent>
       <CardFooter className="flex-col gap-2">
-        {isLinkVisible && (
-          <Button variant="ghost" className="hover:bg-transparent gap-1">
-            You can{" "}
-            <Link href={link} className="text-blue-500">
-              {linkText}
-            </Link>
-          </Button>
-        )}
+        <Button variant="ghost" className="hover:bg-transparent gap-1">
+          You can{" "}
+          <Link href={link} className="text-blue-500">
+            {linkText}
+          </Link>
+        </Button>
       </CardFooter>
     </Card>
   );
