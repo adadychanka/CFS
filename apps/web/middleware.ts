b220dddf944d3a3e4f3fa58769dfb56255c9c78d
Rakhmiddin Asiydinov <rahmiddinasiydinov@gmail.com>
2025-08-28 16:30:10 +0500

    CFS-117: Apply Suggested changes

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,5 +3,7 @@ import { type MiddlewareConfig } from "next/server";
 export { auth as middleware } from "./auth/auth";
 
 export const config: MiddlewareConfig = {
-  matcher: ["/((?!_next/static|_next/image|.*\\.png$|sign-up|admin/log-in).*)"], // _next/static,  _next/image, *.png, sign-up, admin/log-in, these routes won't be blocked by middleware
+  matcher: [
+    "/((?!_next/static|_next/image|.*\\.png$|sign-up|admin/log-in|admin/sign-up).*)",
+  ], // _next/static,  _next/image, *.png, sign-up, admin/log-in, these routes won't be blocked by middleware
 };
