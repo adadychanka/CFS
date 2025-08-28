58d54107f46c04cd6de7820659aa69324a58bd7d
Rakhmiddin Asiydinov <rahmiddinasiydinov@gmail.com>
2025-08-28 13:56:25 +0500

    CFS-119: Show Admin Sign-up for everyone

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -5,5 +5,5 @@ export { auth as middleware } from "./auth/auth";
 export const config: MiddlewareConfig = {
   matcher: [
     "/((?!_next/static|_next/image|.*\\.png$|sign-up|admin/log-in|admin/sign-up).*)",
-  ], // _next/static,  _next/image, *.png, sign-up, admin/log-in, these routes won't be blocked by middleware
+  ], // _next/static,  _next/image, *.png, sign-up, admin/log-in, admin/sign-up these routes won't be blocked by middleware
 };
