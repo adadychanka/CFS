6b10718b76005d45fc7d6e8b3532dbfba4635181
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-28 12:08:52 +0500

    Feature: Connect pagination

--- apps/web/app/(dashboard)/(admin)/admin/page.tsx
+++ apps/web/app/(dashboard)/(admin)/admin/page.tsx
@@ -3,6 +3,7 @@ import { METADATA } from "@/constants/metadata";
 import ChartsSection from "@/components/admin/charts-section";
 import Header from "@repo/ui/components/header";
 import UsersSection from "@/features/users-management/users-section";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -36,8 +37,11 @@ const Page = () => {
       <Header title="Admin Dashboard" />
 
       <div className="w-full max-w-[1280px] mx-auto p-4 @container">
-        <ChartsSection />
-        <UsersSection />
+        {/* TODO: Need custom loader it seems */}
+        <Suspense fallback={"Loading..."}>
+          <ChartsSection />
+          <UsersSection />
+        </Suspense>
       </div>
     </div>
   );
