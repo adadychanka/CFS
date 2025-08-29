c6ff11c0ef7d878b08cbabf3b23d375aab8720c7
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-29 19:31:03 +0500

    Feature: Refactor action calls

--- apps/web/lib/actions/users.ts
+++ apps/web/lib/actions/users.ts
@@ -1,70 +1,19 @@
 "use server";
 
-import { auth } from "@/auth/auth";
-import process from "node:process";
+import callBackend from "@/lib/call-backend";
 
 export async function toggleUserSuspend(userId: string) {
-  const session = await auth();
-  if (!session) {
-    return { success: false, status: 401, message: "Unauthorized" };
-  }
-
-  try {
-    const res = await fetch(
-      `${process.env.BACKEND_API}/api/admin/disable/${userId}`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${session.user.token}`,
-        },
-      },
-    );
-
-    if (!res.ok) {
-      const data = await res.json();
-      return { success: false, status: res.status, message: data.message };
-    }
-
-    return {
-      success: true,
-      status: 201,
-      message: "Feedbacks uploaded successfully",
-    };
-  } catch (e: unknown) {
-    const message = e instanceof Error ? e.message : "Internal Server Error";
-    return { success: false, status: 500, message };
-  }
+  return callBackend(
+    `/api/admin/disable/${userId}`,
+    { method: "POST" },
+    "User suspend toggled successfully",
+  );
 }
 
 export async function disableUser(userId: string) {
-  const session = await auth();
-  if (!session) {
-    return { success: false, status: 401, message: "Unauthorized" };
-  }
-
-  try {
-    const res = await fetch(
-      `${process.env.BACKEND_API}/api/admin/suspend/${userId}`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${session.user.token}`,
-        },
-      },
-    );
-
-    if (!res.ok) {
-      const data = await res.json();
-      return { success: false, status: res.status, message: data.message };
-    }
-
-    return {
-      success: true,
-      status: 201,
-      message: "User has been suspended successfully",
-    };
-  } catch (e: unknown) {
-    const message = e instanceof Error ? e.message : "Internal Server Error";
-    return { success: false, status: 500, message };
-  }
+  return callBackend(
+    `/api/admin/suspend/${userId}`,
+    { method: "POST" },
+    "User has been suspended successfully",
+  );
 }
