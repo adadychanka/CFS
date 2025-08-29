1bc271ca4f8ce74664fe6dc1e8a6f8287d8d2b6e
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-08-29 17:58:10 +0500

    Feature: Disable users and refactor

--- apps/web/lib/actions/users.ts
+++ apps/web/lib/actions/users.ts
@@ -36,3 +36,36 @@ export async function suspendUnsuspendUser(user: User) {
     return { success: false, status: 500, message };
   }
 }
+
+export async function disableUser(userId: string) {
+  const session = await auth();
+  if (!session) {
+    return { success: false, status: 401, message: "Unauthorized" };
+  }
+
+  try {
+    const res = await fetch(
+      `${process.env.BACKEND_API}/api/admin/suspend/${userId}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${session.user.token}`,
+        },
+      },
+    );
+
+    if (!res.ok) {
+      const data = await res.json();
+      return { success: false, status: res.status, message: data.message };
+    }
+
+    return {
+      success: true,
+      status: 201,
+      message: "User has been suspended successfully",
+    };
+  } catch (e: unknown) {
+    const message = e instanceof Error ? e.message : "Internal Server Error";
+    return { success: false, status: 500, message };
+  }
+}
