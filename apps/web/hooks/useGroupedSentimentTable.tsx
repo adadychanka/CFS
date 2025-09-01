3b94269f40c9f7b8da027035e714c6502071eadd
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-01 11:19:26 +0500

    Feature: Add sample data indicator

--- apps/web/hooks/useGroupedSentimentTable.tsx
+++ apps/web/hooks/useGroupedSentimentTable.tsx
@@ -43,7 +43,7 @@ function useGroupedSentimentTable({ data }: Props) {
         </TableCell>
       </TableRow>
     ),
-    [],
+    [isSampleMode],
   );
 
   const { tableHeads, tableRows } = useDynamicTableHeadsAndRows({
