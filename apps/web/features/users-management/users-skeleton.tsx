0fd42d33a2e70406eaa9c77cc70a5b7370a8ade1
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-01 13:59:19 +0500

    Refactor: Optimize code

--- apps/web/features/users-management/users-skeleton.tsx
+++ apps/web/features/users-management/users-skeleton.tsx
@@ -2,20 +2,15 @@ import { TableCell, TableRow } from "@repo/ui/components/table";
 import { Skeleton } from "@repo/ui/components/skeleton";
 
 const UsersSkeleton = () => {
-  return Array.from({ length: 20 }).map((_, index) => (
-    <TableRow key={index}>
-      <TableCell className="px-2 py-4 h-[49px] ">
-        <Skeleton className="h-full w-full rounded-md bg-gray-200" />
-      </TableCell>
-      <TableCell className="px-2 py-4 h-[49px] ">
-        <Skeleton className="h-full w-full rounded-md bg-gray-200" />
-      </TableCell>
-      <TableCell className="px-2 py-4 h-[49px] ">
-        <Skeleton className="h-full w-full rounded-md bg-gray-200" />
-      </TableCell>
-      <TableCell className="px-2 py-4 h-[49px] ">
-        <Skeleton className="h-full w-full rounded-md bg-gray-200" />
-      </TableCell>
+  const COLUMNS = 5;
+
+  return Array.from({ length: 20 }).map((_, rowIndex) => (
+    <TableRow key={rowIndex}>
+      {Array.from({ length: COLUMNS }).map((_, colIndex) => (
+        <TableCell key={colIndex} className="px-2 py-4 h-[49px]">
+          <Skeleton className="h-full w-full rounded-md bg-gray-200" />
+        </TableCell>
+      ))}
     </TableRow>
   ));
 };
