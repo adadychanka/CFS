3c3f7243bdb91a06f272914c4306b19f2df74a3d
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-01 10:00:53 +0500

    Feature: Remove sample route

--- apps/web/components/user-feedback/feedback-table.tsx
+++ apps/web/components/user-feedback/feedback-table.tsx
@@ -27,15 +27,9 @@ type Props = {
 };
 
 const FeedbackTable = ({ currentPage, sentiment, isSampleMode }: Props) => {
-  const fetchURL = isSampleMode
-    ? `/api/feedback/sample?page=${currentPage}&limit=${FEEDBACK_PAGE_LIMIT}`
-    : `/api/feedback?page=${currentPage}&limit=${FEEDBACK_PAGE_LIMIT}`;
-  const { data, error, isLoading, mutate } = useSWR<GetFeedbackResponse>(
-    fetchURL,
+  const { data, error, isLoading } = useSWR<GetFeedbackResponse>(
+    `/api/feedback?page=${currentPage}&limit=${FEEDBACK_PAGE_LIMIT}&sentiment=${sentiment}&isSampleMode=${isSampleMode}`,
     fetcher,
-    {
-      keepPreviousData: true,
-    },
   );
 
   if (error instanceof FetchError) clientAuthGuard(error.status);
