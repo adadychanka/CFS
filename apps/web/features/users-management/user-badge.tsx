8e2aa188d80a510e590df61b5737bf2296a74ed9
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-05 10:54:00 +0500

    Style: Remove admin and user badges

--- apps/web/features/users-management/user-badge.tsx
+++ apps/web/features/users-management/user-badge.tsx
@@ -1,10 +1,8 @@
 import { Badge } from "@repo/ui/components/badge";
 
-type UserBadgeType = "ADMIN" | "USER" | "disabled" | "suspended" | "active";
+type UserBadgeType = "disabled" | "suspended" | "active";
 
 const badgeColors: Record<UserBadgeType, string> = {
-  ADMIN: "bg-purple-200 text-purple-800",
-  USER: "bg-cyan-200 text-cyan-800",
   disabled: "bg-red-200 text-red-800",
   suspended: "bg-yellow-200 text-yellow-800",
   active: "bg-green-200 text-green-800",
