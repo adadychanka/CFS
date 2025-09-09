5a348ed71e8fd57f671da7bccefecb03387e8338
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-09 15:43:52 +0500

    Feature: Migrate to new UI

--- apps/web/features/workspaces/workspace-item.tsx
+++ apps/web/features/workspaces/workspace-item.tsx
@@ -1,9 +1,18 @@
 import {
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@repo/ui/components/sidebar";
-import { Folder } from "lucide-react";
 import WorkspaceItemActions from "@/features/workspaces/workspace-item-actions";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@repo/ui/components/collapsible";
+import { Folder, FolderOpen } from "lucide-react";
+import Link from "next/link";
 
 type Props = {
   name: string;
@@ -11,13 +20,35 @@ type Props = {
 
 const WorkspaceItem = ({ name }: Props) => {
   return (
-    <SidebarMenuItem>
-      <SidebarMenuButton aria-label={`Switch to ${name} workspace`}>
-        <Folder />
-        {name}
-      </SidebarMenuButton>
-      <WorkspaceItemActions />
-    </SidebarMenuItem>
+    <Collapsible className="group/collapsible">
+      <SidebarMenuItem>
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton aria-label={`Switch to ${name} workspace`}>
+            {/* Closed */}
+            <Folder className="h-4 w-4 group-data-[state=closed]/collapsible:block hidden" />
+            {/* Open */}
+            <FolderOpen className="h-4 w-4 group-data-[state=open]/collapsible:block hidden" />
+            {name}
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+        <WorkspaceItemActions />
+
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            <SidebarMenuSubItem>
+              <SidebarMenuSubButton asChild>
+                <Link href="/">Dashboard</Link>
+              </SidebarMenuSubButton>
+            </SidebarMenuSubItem>
+            <SidebarMenuSubItem>
+              <SidebarMenuSubButton asChild>
+                <Link href="/new-sentiment/manual">New Sentiment</Link>
+              </SidebarMenuSubButton>
+            </SidebarMenuSubItem>
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </SidebarMenuItem>
+    </Collapsible>
   );
 };
 
