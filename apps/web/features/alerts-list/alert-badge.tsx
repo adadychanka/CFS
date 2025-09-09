5f7fb3e45f6ba50a66541671a579ed0151f3f1f0
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-09 14:57:57 +0500

    Feature: Refactor code

--- apps/web/features/alerts-list/alert-badge.tsx
+++ apps/web/features/alerts-list/alert-badge.tsx
@@ -1,8 +1,7 @@
-"use client";
-
 import { Badge } from "@repo/ui/components/badge";
+import { SuspiciousActivityAction } from "@/types/suspicious-activity";
 
-const actionColors: Record<string, string> = {
+const actionColors: Record<SuspiciousActivityAction, string> = {
   API: "bg-blue-100 text-blue-800",
   DOWNLOAD: "bg-green-100 text-green-800",
   LOGIN: "bg-yellow-100 text-yellow-800",
@@ -10,7 +9,7 @@ const actionColors: Record<string, string> = {
 };
 
 type AlertBadgeProps = {
-  action: string;
+  action: SuspiciousActivityAction;
 };
 
 const AlertBadge = ({ action }: AlertBadgeProps) => {
