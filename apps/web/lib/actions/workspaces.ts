e6dd0b9dcd334b262d27c07db6eb291d983f82cc
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-10 12:43:13 +0500

    Feature: Handle workspace creation with server actions

---
+++ apps/web/lib/actions/workspaces.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import callBackend from "@/lib/call-backend";
+
+export async function createNewWorkspace(workspaceName: string) {
+  return callBackend(
+    `/api/workspaces`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: { name: workspaceName },
+    },
+    "Workspace created successfully",
+  );
+}
