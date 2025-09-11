555d8f35a8b459b926b87eaf901da44910280275
Rakhmiddin.Asiydinov <rahmiddinasiydinov@example.com>
2025-09-11 18:48:07 +0500

    CFS-167: Update  Admin APIs

--- apps/web/lib/aggregate-sum.ts
+++
@@ -1,9 +0,0 @@
-export function aggregateSum<T extends Record<string, unknown>>(
-  array: T[],
-  key: keyof T,
-): number {
-  return array.reduce((acc, obj) => {
-    const value = obj[key];
-    return acc + (typeof value === "number" ? value : 0);
-  }, 0);
-}
