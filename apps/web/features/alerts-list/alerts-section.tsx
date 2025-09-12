cb80cee5206ec19b5ea04b2a4c4f9922b385a48a
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-12 11:49:19 +0500

    Feature: Move alerts listener up to the context

--- apps/web/features/alerts-list/alerts-section.tsx
+++ apps/web/features/alerts-list/alerts-section.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import AlertsList from "@/features/alerts-list/alerts-list";
-import { useAlertsSocketListener } from "@/hooks/useAlertsListener";
 
 const AlertsSection = () => {
-  useAlertsSocketListener();
-
   return (
     <div className="w-full max-w-[1280px] mx-auto p-4">
       <h2 className="font-medium mb-4">Recent Alerts</h2>
--- apps/web/utils/alerts-helpers.ts
+++ apps/web/utils/alerts-helpers.ts
@@ -1,7 +1,32 @@
+import { toast } from "sonner";
+import type { SuspiciousActivityAlert } from "@/types/suspicious-activity";
+
 export const normilizeIp = (ip: string) => {
   if (ip.startsWith("::ffff:")) {
     return ip.replace("::ffff:", "");
   }
 
   return ip;
+};
+
+/**
+ * Shows a toast notification based on alert type
+ */
+export const handleSuspiciousActivity = (val: SuspiciousActivityAlert) => {
+  switch (val.error) {
+    case "TOO_MANY_API":
+      toast.warning("Too many API requests detected.");
+      break;
+    case "TOO_MANY_DOWNLOAD":
+      toast.warning("Too many downloads detected.");
+      break;
+    case "TOO_MANY_LOGIN":
+      toast.warning("Too many login attempts detected.");
+      break;
+    case "TOO_MANY_UPLOAD":
+      toast.warning("Too many uploads detected.");
+      break;
+    default:
+      toast.warning("Suspicious activity detected.");
+  }
 };
