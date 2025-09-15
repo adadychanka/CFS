671499e4e73eef07d60e863de89856cab93d267a
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-15 15:37:46 +0500

    Feature: Make error page inside the layout

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -2,8 +2,6 @@
 
 import ErrorWholePage from "@/features/error-messages/error-whole-page";
 import { Ghost } from "lucide-react";
-import Link from "next/link";
-import { Button } from "@repo/ui/components/button";
 
 const NotFound = () => {
   return (
@@ -11,13 +9,7 @@ const NotFound = () => {
       Icon={Ghost}
       title="404 - Page not found"
       description="Sorry, we couldn’t find the page you were looking for."
-    >
-      <div className="flex gap-4 flex-wrap justify-center">
-        <Link href="/">
-          <Button>Go back home</Button>
-        </Link>
-      </div>
-    </ErrorWholePage>
+    />
   );
 };
 
--- apps/web/features/error-messages/error-whole-page.tsx
+++ apps/web/features/error-messages/error-whole-page.tsx
@@ -1,5 +1,7 @@
 import { type LucideIcon } from "lucide-react";
 import { type ReactNode } from "react";
+import Link from "next/link";
+import { Button } from "@repo/ui/components/button";
 
 type Props = {
   Icon: LucideIcon;
@@ -9,15 +11,19 @@ type Props = {
 };
 
 /**
- * Component for showing errors as a whole page. Pass more components as children below the content
+ * Component for showing errors as a whole page.
  */
-const ErrorWholePage = ({ Icon, title, description, children }: Props) => {
+const ErrorWholePage = ({ Icon, title, description }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 min-h-screen w-full text-center px-4">
       <Icon className="w-16 h-16 text-neutral-400 mb-4" />
       <h1 className="text-2xl font-bold">{title}</h1>
       <p className="text-neutral-600">{description}</p>
-      {children}
+      <div className="flex gap-4 flex-wrap justify-center">
+        <Link href="/">
+          <Button>Go back home</Button>
+        </Link>
+      </div>
     </div>
   );
 };
