190171e1d8cf22e59ede4bfd06c8da5960c10cd6
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-24 17:46:40 +0500

    Fix: Normilize logo color

--- apps/web/components/logo/sidebar-logo.tsx
+++ apps/web/components/logo/sidebar-logo.tsx
@@ -8,7 +8,7 @@ const SidebarLogo = () => {
       className="flex items-center gap-2 p-2"
       aria-label="Go to homepage"
     >
-      <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+      <div className="bg-neutral-800 text-neutral-200 dark:bg-neutral-200 dark:text-neutral-800 flex aspect-square size-8 items-center justify-center rounded-lg">
         <Brain className="size-4" />
       </div>
       <div className="text-left text-sm leading-tight">
