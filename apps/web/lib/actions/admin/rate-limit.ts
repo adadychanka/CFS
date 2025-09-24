e38a9a3ec9be7564ce7a30210650005bb0cdf1a3
rahmiddinasiydinov <rahmiddinasiydinov@gmail.com>
2025-09-24 19:01:03 +0500

    CFS-142: Fix Issues on rate-limit update form

--- apps/web/lib/actions/admin/rate-limit.ts
+++ apps/web/lib/actions/admin/rate-limit.ts
@@ -3,11 +3,17 @@
 import { auth } from "@/auth/auth";
 import { getServerApi } from "@/lib/server-api";
 import type {
-  RateLimitResponse,
+  RateLimitPatchResponse,
   RateLimitResponseData,
+  RateLimitTarget,
 } from "@/types/rate-limit";
 
 export async function submitRateLimit(rateLimits: RateLimitResponseData[]) {
+  const updateResult: {
+    success: boolean;
+    message: string;
+    target: RateLimitTarget | "Unknown";
+  }[] = [];
   const api = await getServerApi();
 
   try {
@@ -19,28 +25,61 @@ export async function submitRateLimit(rateLimits: RateLimitResponseData[]) {
 
     api.setToken(session.user.token);
 
-    const responses: RateLimitResponse[] = await Promise.all(
+    const responses = await Promise.allSettled(
       rateLimits.map(async (rateLimit) => {
         const res = await api.patch("/api/admins/rate-limit", rateLimit);
-        return await res.json();
+        const result: RateLimitPatchResponse = await res.json();
+        return {
+          success: result.success,
+          message: result.message,
+          target: rateLimit.target,
+        };
       }),
     );
 
-    return {
-      success: true,
-      status: 200,
-      message: "Rate limits updated successfully",
-      results: responses,
-    };
+    responses.map((result) => {
+      if (result.status === "fulfilled") {
+        updateResult.push(result.value);
+      } else {
+        updateResult.push({
+          target: "Unknown",
+          message: "Updating failed",
+          success: false,
+        });
+      }
+    });
+
+    const isAllSuccessful = updateResult.every((response) => response.success);
+    if (isAllSuccessful) {
+      return {
+        success: isAllSuccessful,
+        status: 200,
+        message: "Rate limits updated successfully",
+        results: updateResult,
+      };
+    } else {
+      const isSomeSuccessful = updateResult.some(
+        (response) => response.success,
+      );
+      return {
+        success: isSomeSuccessful,
+        status: 200,
+        message: isSomeSuccessful
+          ? "Not all rate limits updated."
+          : "An error occurred.",
+        results: updateResult,
+      };
+    }
   } catch (error) {
-    console.error("Error updating rate limits:", error);
     if (error instanceof Error) {
       return {
+        success: false,
         status: 500,
         message: error?.message || "Internal server error",
       };
     }
     return {
+      success: false,
       status: 500,
       message: "Internal server error",
     };
