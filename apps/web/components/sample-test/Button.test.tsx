546025a8348ea365375e697b58fed40edb481f8f
Zehn77 <zekhn777@gmail.com>
2025-09-25 17:42:28 +0500

    chore(test): update Vitest + RTL configuration

---
+++ apps/web/components/sample-test/Button.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect } from "vitest";
+import Button from "./Button";
+
+describe("<Button />", () => {
+  test("renders a button with text", () => {
+    render(<Button />);
+
+    const button = screen.getByRole("button", { name: "Click me" });
+
+    expect(button).toBeDefined();
+  });
+});
