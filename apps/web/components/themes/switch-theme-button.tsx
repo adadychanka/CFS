a538cb2aeb4d198d7f958b88809aa4fd3512216d
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2025-09-26 15:01:44 +0500

    Fix: Hydration issue

--- apps/web/components/themes/switch-theme-button.tsx
+++ apps/web/components/themes/switch-theme-button.tsx
@@ -1,9 +1,17 @@
 import { SidebarMenuButton } from "@repo/ui/components/sidebar";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const SwitchThemeButton = () => {
   const { setTheme, resolvedTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
 
   const handleToggleThemeNav = () => {
     setTheme(resolvedTheme === "light" ? "dark" : "light");
