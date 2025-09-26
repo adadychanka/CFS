4a73f8aac5b3e0e37fc1beb87446ff6bf59a5caa
Zehn77 <zekhn777@gmail.com>
2025-09-26 16:15:22 +0500

    refactor: implement all suggested changes from PR review

--- apps/web/components/sample-test/Button.test.tsx
+++ apps/web/components/sample-test/Button.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
-import "@testing-library/jest-dom";
 import Button from "./Button";
 
 describe("<Button />", () => {
